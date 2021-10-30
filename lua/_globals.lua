f4a65d1d58dbd1d12428edfb4c874a3d2ea4f7ab
JavierPoduje <javier.poduje@gmail.com>
2021-10-30 16:02:39 -0300

    Add functions JsonToPhp and PhpToJson

--- lua/_globals.lua
+++ lua/_globals.lua
@@ -25,5 +25,25 @@ SplitAndMoveToPrevBuffer = function()
   vim.api.nvim_exec(":BufferLineCyclePrev", true)
 end
 
+-- Convert json to php array
+JsonToPhp = function()
+  vim.api.nvim_command([[ :%s/\%V":\s*\"/" => "/ge ]])
+  vim.api.nvim_command([[ :%s/\%V":\s*{/" => [/ge ]])
+  vim.api.nvim_command([[ :%s/\%V":\s*\[/" => [/ge ]])
+  vim.api.nvim_command([[ :%s/\%V{/[/ge ]])
+  vim.api.nvim_command([[ :%s/\%V}/]/ge ]])
+  vim.api.nvim_command([[ :%s/\%V":\s*/" => /ge ]])
+end
+-- Conver php array to json
+-- TODO: identify lists properly
+PhpToJson = function()
+  vim.api.nvim_command([[ :%s/\%V"\s*=>\s*"/": "/ge ]])
+  vim.api.nvim_command([[ :%s/\%V"\s*=>\s*\[/": {/ge ]])
+  vim.api.nvim_command([[ :%s/\%V"\s*=>\s*\[/": [/ge ]])
+  vim.api.nvim_command([[ :%s/\%V\[/{/ge ]])
+  vim.api.nvim_command([[ :%s/\%V]/}/ge ]])
+  vim.api.nvim_command([[ :%s/\%V"\s*=>\s*/": /ge ]])
+end
+
 -- avoid lsp annoying warning message...
 vim = vim
