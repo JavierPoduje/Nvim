8c7330eced7281cb3662e28b9a98ed49faef025a
JavierPoduje <javier.poduje@gmail.com>
2022-02-06 13:21:41 -0300

    Add function to wipe all registers

--- lua/_globals.lua
+++ lua/_globals.lua
@@ -2,9 +2,9 @@ Opts = { noremap = true, silent = true }
 NoisyOpts = { noremap = true, silent = false }
 
 -- Print inspected version of something
-P = function(something)
-	print(vim.inspect(something))
-	return something
+P = function(x)
+	print(vim.inspect(x))
+	return x
 end
 
 -- Convert json to php array
@@ -15,6 +15,17 @@ JsonToPhp = function()
 	vim.api.nvim_command([[ :%s/\%V{/[/ge ]])
 	vim.api.nvim_command([[ :%s/\%V}/]/ge ]])
 	vim.api.nvim_command([[ :%s/\%V":\s*/" => /ge ]])
+end
+
+-- Clean all the alphanumeric registers
+WipeRegisters = function()
+  for i = 34, 122 do
+    local char = vim.fn.nr2char(i)
+    if char:match("%w") then
+      vim.fn.setreg(char, {})
+    end
+  end
+  vim.api.nvim_command([[ :wshada! ]])
 end
 
 v = vim.api
