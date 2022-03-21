741e7524160805d7b386fd9738d30e4416b8f2ee
JavierPoduje <javier.poduje@gmail.com>
2022-03-21 20:53:08 -0300

    Refactor buffero

--- lua/config/buffero.lua
+++ lua/config/buffero.lua
@@ -1,4 +1,3 @@
-local g = require("g")
 require("buffero")
 
-g.n_silent_map("<Leader>bñ", ":lua require'buffero'.open_last_closed_buffer()<CR>")
+require("g").n_silent_map("<Leader>bñ", ":lua require'buffero'.open_last_closed_buffer()<CR>")
