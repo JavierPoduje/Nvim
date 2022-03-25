193e50f0c847e8246bcc1c059d35cd2637978142
JavierPoduje <javier.poduje@gmail.com>
2022-03-25 08:38:33 -0300

    Refactor harpoon

--- lua/config/harpoon.lua
+++ lua/config/harpoon.lua
@@ -12,19 +12,10 @@ require("harpoon").setup({
 -- add file to harpoon
 g.n_silent_map("<Leader>ya", ":lua require('harpoon.mark').add_file()<CR>")
 
--- add file to harpoon
+-- toggle harpoon's ui
 g.n_silent_map("<Leader>yt", ":lua require('harpoon.ui').toggle_quick_menu()<CR>")
 
--- set goto's
--- Six files should be more than enough...
-local chars_to_nums = {
-	v = 1,
-	c = 2,
-	x = 3,
-	f = 4,
-	d = 5,
-	s = 6,
-}
-for char, num in pairs(chars_to_nums) do
+-- navegate harpoon's files
+for char, num in pairs(g.left_num_by_char) do
 	g.n_silent_map("<Leader>y" .. char, ":lua require('harpoon.ui').nav_file(" .. tostring(num) .. ")<CR>")
 end
