50c1faa3c66ca817d26a2853d6de2b44808afa58
JavierPoduje <javier.poduje@gmail.com>
2023-02-18 10:43:39 -0300

    feat: replace lsp-installer with mason

--- lua/config/lsp/init.lua
+++
@@ -1,2 +0,0 @@
-require("config.lsp.lsp-installers")
-require("config.lsp.handlers").setup()
---
+++ lua/config/lspconfig.lua
@@ -0,0 +1,84 @@
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap = true, silent = true }
+
+local signs = {
+  { name = "DiagnosticSignError", text = "" },
+  { name = "DiagnosticSignWarn",  text = "" },
+  { name = "DiagnosticSignHint",  text = "" },
+  { name = "DiagnosticSignInfo",  text = "" },
+}
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = { active = signs },
+  update_in_insert = false,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focusable = false,
+    style = "minimal",
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
+})
+
+vim.keymap.set("n", "<C-k>", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "<leader>[", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "<leader>]", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<space>gq", vim.diagnostic.setloclist, opts)
+
+local on_attach = function(client, bufnr)
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+  vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set("n", "<space>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<leader>R", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, bufopts)
+  vim.keymap.set("n", "<leader>gf", function()
+    vim.lsp.buf.format({ async = true })
+  end, bufopts)
+end
+
+local lsp_flags = {
+  debounce_text_changes = 150,
+}
+
+require("lspconfig")["pyright"].setup({
+  on_attach = on_attach,
+  flags = lsp_flags,
+})
+require("lspconfig")["intelephense"].setup({
+  on_attach = on_attach,
+  flags = lsp_flags,
+})
+require("lspconfig")["cssls"].setup({
+  on_attach = on_attach,
+  flags = lsp_flags,
+})
+require("lspconfig")["tsserver"].setup({
+  on_attach = on_attach,
+  flags = lsp_flags,
+})
+require("lspconfig")["lua_ls"].setup({
+  on_attach = on_attach,
+  flags = lsp_flags,
+})
+require("lspconfig")["rust_analyzer"].setup({
+  on_attach = on_attach,
+  flags = lsp_flags,
+  settings = {
+    ["rust-analyzer"] = {},
+  },
+})
