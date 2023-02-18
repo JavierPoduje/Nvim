04adcc4f8e8efe77631ee02075c0043a7355cbe4
JavierPoduje <javier.poduje@gmail.com>
2023-02-18 11:48:39 -0300

    fix: lua identation

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -7,24 +7,24 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  update_in_insert = false,
-  underline = true,
-  severity_sort = true,
-  float = {
-    focusable = false,
-    style = "minimal",
-    border = "rounded",
-    source = "always",
-    header = "",
-    prefix = "",
-  },
+	virtual_text = true,
+	signs = true,
+	update_in_insert = false,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
 })
 
 vim.keymap.set("n", "<C-k>", vim.diagnostic.open_float, opts)
@@ -33,54 +33,55 @@ vim.keymap.set("n", "<leader>]", vim.diagnostic.goto_next, opts)
 vim.keymap.set("n", "<space>gq", vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-  vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-  vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set("n", "<space>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set("n", "<leader>R", vim.lsp.buf.rename, bufopts)
-  vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, bufopts)
-  vim.keymap.set("n", "<leader>gf", function()
-    vim.lsp.buf.format({ async = true })
-  end, bufopts)
+	vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "<leader>R", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, bufopts)
+	vim.keymap.set("n", "<leader>ff", function()
+		vim.lsp.buf.format({ async = true })
+	end, bufopts)
 end
 
 local lsp_flags = {
-  debounce_text_changes = 150,
+	debounce_text_changes = 150,
 }
 
 require("lspconfig")["pyright"].setup({
-  on_attach = on_attach,
-  flags = lsp_flags,
+	on_attach = on_attach,
+	flags = lsp_flags,
 })
 require("lspconfig")["intelephense"].setup({
-  on_attach = on_attach,
-  flags = lsp_flags,
+	on_attach = on_attach,
+	flags = lsp_flags,
 })
 require("lspconfig")["cssls"].setup({
-  on_attach = on_attach,
-  flags = lsp_flags,
+	on_attach = on_attach,
+	flags = lsp_flags,
 })
 require("lspconfig")["tsserver"].setup({
-  on_attach = on_attach,
-  flags = lsp_flags,
+	on_attach = on_attach,
+	flags = lsp_flags,
 })
+
 require("lspconfig")["lua_ls"].setup({
-  on_attach = on_attach,
-  flags = lsp_flags,
+	on_attach = on_attach,
+	flags = lsp_flags,
 })
 require("lspconfig")["rust_analyzer"].setup({
-  on_attach = on_attach,
-  flags = lsp_flags,
-  settings = {
-    ["rust-analyzer"] = {},
-  },
+	on_attach = on_attach,
+	flags = lsp_flags,
+	settings = {
+		["rust-analyzer"] = {},
+	},
 })
